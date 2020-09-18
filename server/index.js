a8b36e3b07a8569f83375b80d248dc78142b51c8
jacobmcfadden <jacobmcfadden@protonmail.com>
2020-09-18 12:42:11 -0500

    initial group setup complete

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+
+const app = express();
+
+const {CONNECTION_STRING, SESSION_SECRET, SERVER_PORT}=process.env;
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24},
+    secret: SESSION_SECRET
+}));
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then( db => {
+    app.set('db', db)
+    console.log('Connected to db')
+}).catch(err => console.log(err));
+
+app.listen(SERVER_PORT, () => console.log(`Connected to port ${SERVER_PORT}`));
