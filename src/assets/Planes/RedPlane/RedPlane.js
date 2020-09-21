5a5730d18b26c72bcb5aa2c34f08605141152646
jacobmcfadden <jacobmcfadden@protonmail.com>
2020-09-21 11:00:35 -0500

    Went back 2 commits, finished styling setup and added assets

---
+++ src/assets/Planes/RedPlane/RedPlane.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import './RedPlane.scss';
+
+function RedPlane(props) {
+  	return (
+      <svg 
+      viewBox="0 0 238.7 138.7" 
+      className="RedPlane"
+      {...props}
+      >
+      <style />
+      <defs>
+      </defs>
+      <g id="Group_17654_1_">
+        <path id="Path_7644_2_" className="RedPlane0" d="M48.9,80.5c0,0-10.8,39.7-8.9,53.4L238.7,0l-7.9,2.8L48.9,80.5z"/>
+        <path id="Path_7645_2_" className="RedPlane1" d="M0,50.5L238.7,0L49.2,83.7L0,50.5z"/>
+        <path id="Path_7646_2_" className="RedPlane2" d="M69.9,98.6C66.9,107.1,40,134,40,134l54.4-26.4C94.4,107.6,75.8,95.8,69.9,98.6z"/>
+        <path id="Path_7647_2_" className="RedPlane1" d="M69.9,98.6L238.7,0L124.9,138.7L69.9,98.6z"/>
+      </g>
+      </svg>
+    );
+}
+
+export default RedPlane;
