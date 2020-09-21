f8f2b804b79548f197b764e1dba901f296cfbe38
jacobmcfadden <jacobmcfadden@protonmail.com>
2020-09-21 11:47:43 -0500

    Setup the welcome page banner and nav styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-
+      <Welcome/>
     </div>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import '../styling/components/Welcome.scss';
+
+import CloudSpan from '../assets/Clouds/CloudSpan/CloudSpan';
+import PaperPlanes from '../assets/Planes/PaperPlanes/PaperPlanes';
+import Zeppi from '../assets/ZeppiLogo/Zeppi/Zeppi';
 
 function Welcome() {
     return (
         <div>
             <div className="cloud-container">
                 <div className="nav-bar">
-                    <div className="logo"></div>
+                    <Zeppi className="zeppi-text"/>
                     <div className="header-buttons">
-                        <button>LOGIN</button>
-                        <button>SIGN UP</button>
+                        <div className="container__row">
+                        <div className="login-button">
+                            <p>LOGIN</p>
+                        </div>
+                        <div className="signup-button">
+                            <p>SIGNUP</p>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className="preview-container">
-                <div>
+                <div className="container__row">
+                    <PaperPlanes className="paper-planes"/>
+                </div>
+                <CloudSpan className="CloudSpan"/>
+                <div className="RightBelowCloud">
 
                 </div>
             </div>
