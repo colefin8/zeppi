db69faed2857078dbbb6eb3e4b6e7b066d630a29
Brenna Harden <jupitereyes@Brennas-MacBook-Air.local>
2020-09-24 09:06:20 -0600

    resolved a couple of errors

--- db/create_message.sql
+++ db/create_message.sql
@@ -1,8 +1,8 @@
 INSERT INTO messages
     (message, sender, receiver, lat, long)
 VALUES
-    ($1, $2, $3)
-RETURNING *;
+    ($1, $2, $3, $4, $5)
+
 
 -- SELECT * FROM users
 -- JOIN messages ON(messages.sender = users.user_id)
