6723f9fc5181ee60e604c29b101557df9434bfb9
karenChernandez <karenChernandez@live.com>
2020-09-29 16:20:20 -0700

    updatingCounters

--- db/create_user.sql
+++ db/create_user.sql
@@ -1,5 +1,5 @@
 INSERT INTO users
-    (user_name, first_name, last_name, phone_number, email, password)
+    (user_name, first_name, last_name, phone_number, email, password, total_drops, total_loot)
 VALUES
-    ($1, $2, $3, $4, $5, $6)
+    ($1, $2, $3, $4, $5, $6, 0, 0)
 RETURNING *;
